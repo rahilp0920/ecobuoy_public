98531ad82d9e7a7bab24cb5fe78739583fb34130
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2025-05-25 16:21:39 -0400

    Update RTL.js

--- src/layouts/RTL.js
+++ src/layouts/RTL.js
@@ -1,4 +1,3 @@
-import React from 'react';// Chakra imports
 import { ChakraProvider, Portal, useDisclosure } from '@chakra-ui/react';
 import { RtlProvider } from 'components/RTLProvider/RTLProvider';
 import Configurator from 'components/Configurator/Configurator';
