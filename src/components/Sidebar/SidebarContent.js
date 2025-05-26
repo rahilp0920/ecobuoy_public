af653b7f93e615f5fe0a46ed24d965b44770cafc
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2025-05-26 18:08:42 -0400

    Update SidebarContent.js

--- src/components/Sidebar/SidebarContent.js
+++ src/components/Sidebar/SidebarContent.js
@@ -1,25 +1,159 @@
+/*eslint-disable*/
+// chakra imports
 import {
-    Box,
-    Button, Flex,
-    Link,
-    Stack,
-    Text,
-    useColorModeValue,
-    Image
+  Box,
+  Button,
+  Flex,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+  Image
 } from "@chakra-ui/react";
 import IconBox from "components/Icons/IconBox";
-// import { CreativeTimLogo } from "components/Icons/Icons";
 import { Separator } from "components/Separator/Separator";
 import { SidebarHelp } from "components/Sidebar/SidebarHelp";
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import logo from "assets/logo.png"; // <-- Import your logo
 
-// ...rest of your code...
-
+// This function creates the links and collapses that appear in the sidebar (left menu)
 const SidebarContent = ({ logoText, routes }) => {
-  // ...rest of your code...
+  let location = useLocation();
+  const [state, setState] = React.useState({});
+
+  // Verifies if routeName is the one active (in browser input)
+  const activeRoute = (routeName) => {
+    return location.pathname === routeName ? "active" : "";
+  };
+  const createLinks = (routes) => {
+    // Chakra Color Mode
+    const activeBg = useColorModeValue("white", "gray.700");
+    const inactiveBg = useColorModeValue("white", "gray.700");
+    const activeColor = useColorModeValue("gray.700", "white");
+    const inactiveColor = useColorModeValue("gray.400", "gray.400");
+
+    return routes.map((prop, key) => {
+      if (prop.redirect) {
+        return null;
+      }
+      if (prop.category) {
+        var st = {};
+        st[prop["state"]] = !state[prop.state];
+        return (
+          <div key={prop.name}>
+            <Text
+              color={activeColor}
+              fontWeight="bold"
+              mb={{ xl: "12px" }}
+              mx="auto"
+              ps={{ sm: "10px", xl: "16px" }}
+              py="12px"
+            >
+              {document.documentElement.dir === "rtl"
+                ? prop.rtlName
+                : prop.name}
+            </Text>
+            {createLinks(prop.views)}
+          </div>
+        );
+      }
+      return (
+        <NavLink to={prop.layout + prop.path} key={prop.name}>
+          {activeRoute(prop.layout + prop.path) === "active" ? (
+            <Button
+              boxSize="initial"
+              justifyContent="flex-start"
+              alignItems="center"
+              bg={activeBg}
+              mb={{ xl: "12px" }}
+              mx={{ xl: "auto" }}
+              ps={{ sm: "10px", xl: "16px" }}
+              py="12px"
+              borderRadius="15px"
+              _hover="none"
+              w="100%"
+              _active={{
+                bg: "inherit",
+                transform: "none",
+                borderColor: "transparent",
+              }}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              <Flex>
+                {typeof prop.icon === "string" ? (
+                  <Icon>{prop.icon}</Icon>
+                ) : (
+                  <IconBox
+                    bg="teal.300"
+                    color="white"
+                    h="30px"
+                    w="30px"
+                    me="12px"
+                  >
+                    {prop.icon}
+                  </IconBox>
+                )}
+                <Text color={activeColor} my="auto" fontSize="sm">
+                  {document.documentElement.dir === "rtl"
+                    ? prop.rtlName
+                    : prop.name}
+                </Text>
+              </Flex>
+            </Button>
+          ) : (
+            <Button
+              boxSize="initial"
+              justifyContent="flex-start"
+              alignItems="center"
+              bg="transparent"
+              mb={{ xl: "12px" }}
+              mx={{ xl: "auto" }}
+              py="12px"
+              ps={{ sm: "10px", xl: "16px" }}
+              borderRadius="15px"
+              _hover="none"
+              w="100%"
+              _active={{
+                bg: "inherit",
+                transform: "none",
+                borderColor: "transparent",
+              }}
+              _focus={{
+                boxShadow: "none",
+              }}
+            >
+              <Flex>
+                {typeof prop.icon === "string" ? (
+                  <Icon>{prop.icon}</Icon>
+                ) : (
+                  <IconBox
+                    bg={inactiveBg}
+                    color="teal.300"
+                    h="30px"
+                    w="30px"
+                    me="12px"
+                  >
+                    {prop.icon}
+                  </IconBox>
+                )}
+                <Text color={inactiveColor} my="auto" fontSize="sm">
+                  {document.documentElement.dir === "rtl"
+                    ? prop.rtlName
+                    : prop.name}
+                </Text>
+              </Flex>
+            </Button>
+          )}
+        </NavLink>
+      );
+    });
+  };
+
   const links = <>{createLinks(routes)}</>;
+
   return (
     <>
       <Box pt={"25px"} mb="12px">
@@ -34,20 +168,19 @@ const SidebarContent = ({ logoText, routes }) => {
           alignItems="center"
           fontSize="11px"
         >
-          {/* Replace the CreativeTimLogo with your own logo */}
           <Image src={logo} alt="Ecobuoy Logo" boxSize="32px" me="10px" />
           <Text fontSize="sm" mt="3px">
             Ecobuoy
           </Text>
         </Link>
-        <Separator></Separator>
+        <Separator />
       </Box>
       <Stack direction="column" mb="40px">
         <Box>{links}</Box>
       </Stack>
       <SidebarHelp />
     </>
-  )
-}
+  );
+};
 
-export default SidebarContent
+export default SidebarContent;
