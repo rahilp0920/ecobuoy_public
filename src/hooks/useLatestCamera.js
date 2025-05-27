2b3782d1f796e90b6020739b0d43b31716724d53
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2025-05-27 12:09:57 -0400

    Create useLatestCamera.js

---
+++ src/hooks/useLatestCamera.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export default function useLatestCamera() {
+  const [data, setData] = useState({ image: '', timestamp: '' });
+
+  const fetchImage = async () => {
+    const res = await fetch('http://localhost:5000/api/latest-camera');
+    const json = await res.json();
+    setData(json);
+  };
+
+  useEffect(() => {
+    fetchImage();
+    const interval = setInterval(fetchImage, 2000); // refresh every 2 seconds
+    return () => clearInterval(interval);
+  }, []);
+
+  return data;
+}
