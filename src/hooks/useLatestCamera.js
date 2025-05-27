4b1233169b8818d640ba12bba441db20044cc2b5
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2025-05-27 13:00:41 -0400

    Update useLatestCamera.js

--- src/hooks/useLatestCamera.js
+++ src/hooks/useLatestCamera.js
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react';
-
 export default function useLatestCamera() {
   const [data, setData] = useState({ image: '', timestamp: '' });
 
   const fetchImage = async () => {
     const res = await fetch('http://localhost:5000/api/latest-camera');
     const json = await res.json();
+    if (json.image) {
+      json.image = `http://localhost:5000/api${json.image}`;
+    }
     setData(json);
   };
 
