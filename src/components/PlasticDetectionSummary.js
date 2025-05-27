585dd26e48ebbf7749952698cbc2d362113b41e8
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2025-05-27 14:31:29 -0400

    Update PlasticDetectionSummary.js

--- src/components/PlasticDetectionSummary.js
+++ src/components/PlasticDetectionSummary.js
@@ -1,20 +1,41 @@
 import React from "react";
-import { Box, Text, Flex, Progress } from "@chakra-ui/react";
+import { Box, Text, SimpleGrid, Stat, StatLabel, StatNumber } from "@chakra-ui/react";
 
-const PlasticDetectionSummary = ({ data }) => (
-  <Box bg="white" boxShadow="lg" rounded="xl" p="6" my="6" w="100%">
-    <Text fontWeight="bold" color="teal.600" mb="4">Plastic Detection Summary</Text>
-    <Flex direction="column" gap="3">
-      {data.map((item, i) => (
-        <Flex align="center" key={i}>
-          <Text fontSize="2xl" mr="3">{item.icon}</Text>
-          <Text fontWeight="semibold" minW="60px">{item.type}</Text>
-          <Progress value={item.percent} colorScheme="blue" flex="1" mx="4" borderRadius="md" />
-          <Text fontSize="md" ml="2">{item.count} <span style={{color:'#aaa'}}>{item.percent}%</span></Text>
-        </Flex>
-      ))}
-    </Flex>
-  </Box>
-);
+const ICONS = {
+  bottle: "🍼",
+  bag: "🛍️",
+  other: "🔎"
+};
+
+/**
+ * PlasticDetectionSummary
+ * @param {object} props
+ * @param {object} props.data - { bottle: number, bag: number, other: number }
+ */
+const PlasticDetectionSummary = ({ data }) => {
+  const { bottle = 0, bag = 0, other = 0 } = data || {};
+
+  return (
+    <Box bg="white" boxShadow="md" p={4} rounded="lg" mb={6}>
+      <Text fontWeight="bold" mb={2} color="teal.600">
+        Plastic Detection Summary
+      </Text>
+      <SimpleGrid columns={3} spacing={4} mb={2}>
+        <Stat>
+          <StatLabel>Bottles {ICONS.bottle}</StatLabel>
+          <StatNumber>{bottle}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>Bags {ICONS.bag}</StatLabel>
+          <StatNumber>{bag}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>Other {ICONS.other}</StatLabel>
+          <StatNumber>{other}</StatNumber>
+        </Stat>
+      </SimpleGrid>
+    </Box>
+  );
+};
 
 export default PlasticDetectionSummary;
