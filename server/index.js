054f55ca276e75d9b722e6aec928b33c829ae674
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2025-05-27 14:07:14 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+
 const cameraRouter = require('./camera');
+const dataRouter = require('./data');
 
 const app = express();
 const PORT = 5000;
 
-const dataRouter = require('./data');
-app.use('/api', dataRouter);
-
+// Enable CORS for all routes
 app.use(cors());
+
+// Mount both routers at /api
 app.use('/api', cameraRouter);
+app.use('/api', dataRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
